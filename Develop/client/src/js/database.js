cdc15bb9382d8af9b3c0c1601717f8fde79f90bf
Zac Smart <zac_chaz@outlook.com>
2022-12-11 19:15:48 +1100

    finished database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,26 +14,26 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-
-  const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readwrite');
-  const store = tx.objectStore('todos');
-  const request = store.add({ todo: content });
+console.log("put to the Database");
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-
-console.error('putDb not implemented');
-}
+};
+ 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-const todosDb = await openDB('todos', 1);
-  const tx = todosDb.transaction('todos', 'readonly');
-  const store = tx.objectStore('todos');
-  const request = store.getAll();
+  console.log("Get from the database");
+const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
-console.error('getDb not implemented');
-}
+  return result?.value;
+};
+
 initdb();
